3945f426ea68721b4e3e0de9150d3c59f5e13eb7
dongpangcheckm8 <dongpangcheckm8@gmail.com>
2019-06-14 23:17:42 +0900

    react tutorial #1 -- how to make components with react

---
+++ react_tutorial_example/src/App.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import './App.css';
+import TOC from './components/TOC'
+import Content from './components/Content'
+import Subject from './components/Subject'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      mode:'welcome',
+      subject: {title:'WEB', sub:'world wide web'},
+      welcome:{title:'WELCOME', desc:'Hello, react'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is HyperText Markup Language'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+  render() {
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    return (
+      <div className="App">
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
+      </div>
+    );
+  }
+}
+
+export default App;
